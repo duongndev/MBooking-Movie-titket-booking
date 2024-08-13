0b0a52c5112af4a59d064bf3448e5ab769f5b541
duongnd <77197933+ngduonng@users.noreply.github.com>
2024-08-13 20:17:57 +0700

    init commit

--- MBookingApp/app/src/main/java/com/duongnd/mbookingapp/ui/movie/VideoTrailerActivity.kt
+++ MBookingApp/app/src/main/java/com/duongnd/mbookingapp/ui/movie/VideoTrailerActivity.kt
@@ -1,21 +1,40 @@
 package com.duongnd.mbookingapp.ui.movie
 
+import android.net.Uri
 import android.os.Bundle
+import android.widget.MediaController
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.duongnd.mbookingapp.R
+import com.duongnd.mbookingapp.databinding.ActivityVideoTrailerBinding
 
 class VideoTrailerActivity : AppCompatActivity() {
+
+    private var _binding: ActivityVideoTrailerBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_video_trailer)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        _binding = ActivityVideoTrailerBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val videoUrl = intent.getStringExtra("VIDEO_URL")
+        val videoUri = Uri.parse(videoUrl)
+        val mediaController = MediaController(this)
+        mediaController.setAnchorView(binding.videoViewTrailer)
+        binding.videoViewTrailer.setMediaController(mediaController)
+        binding.videoViewTrailer.setVideoURI(videoUri)
+        binding.videoViewTrailer.start()
+
+        binding.videoViewTrailer.setOnCompletionListener {
+           onBackPressedDispatcher.onBackPressed()
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
