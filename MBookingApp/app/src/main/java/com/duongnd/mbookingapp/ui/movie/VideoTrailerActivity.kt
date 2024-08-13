8426306e150ffcd00620f50ee27252c395f0cfb5
duongnd <77197933+ngduonng@users.noreply.github.com>
2024-08-13 20:16:36 +0700

    init commit

---
+++ MBookingApp/app/src/main/java/com/duongnd/mbookingapp/ui/movie/VideoTrailerActivity.kt
@@ -0,0 +1,21 @@
+package com.duongnd.mbookingapp.ui.movie
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.duongnd.mbookingapp.R
+
+class VideoTrailerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_video_trailer)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
